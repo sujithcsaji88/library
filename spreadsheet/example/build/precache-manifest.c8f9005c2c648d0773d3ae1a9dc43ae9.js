9bc8c06c5996c8bd355473c7067df0378c3a0b6f
Abhilash R Jayakumar <62945844+abhlashrj@users.noreply.github.com>
2020-07-12 22:05:47 -0400

    Spreadsheet library build files

---
+++ spreadsheet/example/build/precache-manifest.c8f9005c2c648d0773d3ae1a9dc43ae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47385427b8cd396b35158477175067e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f9584c0fcd2ff0b82d9",
+    "url": "./static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "6f9584c0fcd2ff0b82d9",
+    "url": "./static/js/2.dc94ffce.chunk.js"
+  },
+  {
+    "revision": "25b44fdcb97e8bc084e57e9e3d4ad514",
+    "url": "./static/js/2.dc94ffce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746d46ca51c4bf7c2701",
+    "url": "./static/js/main.d0de3309.chunk.js"
+  },
+  {
+    "revision": "5dbf7aa74148ffd4e16a",
+    "url": "./static/js/runtime-main.cb6cab9a.js"
+  }
+]);
