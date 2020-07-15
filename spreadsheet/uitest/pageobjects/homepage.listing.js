258602e342777a2f8c9ea8074f4d86c4da251874
Jijo Varghese <jijo@Jijos-MacBook-Air.local>
2020-07-15 09:25:42 +0530

    Adding fligthederelement

--- spreadsheet/uitest/pageobjects/homepage.listing.js
+++ spreadsheet/uitest/pageobjects/homepage.listing.js
@@ -2,6 +2,7 @@ import Page from '../common/page.js'
 
 class Homepage extends Page {
     get recordCOuntElement() { return $('//*[@class="totalCount"]') }
+    get recordCOuntElement() { return $('//*[@class="totalCount"]') }
     open() {
         super.open('');
     }
